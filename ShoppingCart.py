1e1421ad0c88a7a638c965dc587b320905e7a01a
Kerwin Charles <KerwinCharles3@gmail.com>
2020-06-17 00:26:36 -0400

    working code

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -5,22 +5,30 @@ class ShoppingCart:
 		self._items = {}
 		
 	def addItem(self, item, quantity, price):
-		self._total +=  quantity * price
+		self._total += quantity * price
 		self._items[item] = quantity
 		
-	def removeItem(self, item):
-		self._total -= quantity * price 
-		self._items[item] = quantity
+	def removeItem(self, item, price):
+		self._total -= price
+		self._items[item] -= 1
 		
-	def checkout(self):
-		pass
+	def checkout(self, payment):
+		if payment < self._total:
+			print("Need More Cash")
+		else:
+			change = payment - self._total
+			print("Please collect your change: ${}".format(change))
 		
 		
 Cart1 = ShoppingCart()
 
 Cart1.addItem("broccoli", 3, 6)
 print(Cart1._items)
+Cart1.removeItem("broccoli" , 6)
 print(Cart1._total)
+Cart1.checkout(20)
+
+
 
 
 
